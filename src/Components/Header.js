781f241842d39424a70f0ecf916f3f882f238756
Justin Russell <Justin@justins-macbook.lan>
2020-09-03 15:14:36 -0700

    changing colors

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -17,38 +17,60 @@ class Header extends Component {
 
     return (
       <header id="home">
-      <ParticlesBg type="random" bg={true} />
-      <nav id="nav-wrap">
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+        <ParticlesBg type="random" color="#3973ac" bg={true} />
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+            Show navigation
+          </a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">
+            Hide navigation
+          </a>
 
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         {/* <li><a className="smoothscroll" href="#resume">Resume</a></li> */}
-            <li><a className="smoothscroll" href="#portfolio">Work</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
-      </nav>
+          <ul id="nav" className="nav">
+            <li className="current">
+              <a className="smoothscroll" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#about">
+                About
+              </a>
+            </li>
+            {/* <li><a className="smoothscroll" href="#resume">Resume</a></li> */}
+            <li>
+              <a className="smoothscroll" href="#portfolio">
+                Work
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
 
-      <div className="row banner">
-      
-         <div className="banner-text">
+        <div className="row banner">
+          <div className="banner-text">
             <h1 className="responsive-headline">{name}</h1>
             <h3>{description}.</h3>
             <hr />
             <ul className="social">
-               {/* <a href={project} className="button btn project-btn"><i className="fa fa-book"></i>Project</a> */}
-               <a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
+              {/* <a href={project} className="button btn project-btn"><i className="fa fa-book"></i>Project</a> */}
+              <a href={github} className="button btn github-btn">
+                <i className="fa fa-github"></i>Github
+              </a>
             </ul>
-         </div>
-      </div>
+          </div>
+        </div>
 
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
-
-   </header>
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+      </header>
     );
   }
 }
