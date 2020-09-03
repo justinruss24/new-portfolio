1d9d302275b79b618f34263b9e7553e23269aed9
Justin Russell <Justin@justins-macbook.lan>
2020-09-03 15:18:23 -0700

    debug

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -28,7 +28,7 @@ class Resume extends Component {
       // })
 
       var skills = this.props.data.skills.map((skills)=>{
-        var className = 'bar-expand '+skills.name.toLowerCase();
+      var className = 'bar-expand '+skills.name.toLowerCase();
         return (
           <li key={skills.name}>
             {/* <span style={{width:skills.level, backgroundColor:this.getRandomColor()}}className={className}></span> */}
